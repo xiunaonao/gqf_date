553707bc5aca2f2ebd655b13f63de8f9ad487695
xiunaonao <onlymyalways@163.com>
2018-06-20 10:24:23 +0800

    相亲

---
+++ public/js/sys/router.js
@@ -0,0 +1,82 @@
+var routes=[
+	{
+		url:'/',
+		dom:'home',
+		script:'home.js'
+	},
+	{
+		url:'detail/{id}',
+		dom:'detail',
+		script:'detail.js'
+	},
+	{
+		url:'editor',
+		dom:'editor',
+		script:'editor.js'
+	},
+	{
+		url:'list/{page}',
+		dom:'list',
+		script:'list.js'
+	},
+	{
+		url:'filter',
+		dom:'filter',
+		script:'filter.js'
+	}
+	
+]
+
+window.linkTo=function(url){
+	var hash=location.hash;
+	for(var i=0;i<routes.length;i++){
+		getData(routes[i]);
+	}
+
+
+	function getData(obj){
+		var param=obj.url.toLowerCase().split('/');
+		var hashs=hash.toLowerCase().split('/');
+		if('#'+param[0]==hashs[0]){
+			var paramObj={};
+			for(var i=0;i<param.length;i++){
+				if(i==0)
+					continue;
+				paramObj[param[i].replace('{','').replace('}','')]=hashs.length>(i)?hashs[i]:'';
+				console.log(param[i].replace('{','').replace('}','')+' = '+ (hashs.length>(i)?hashs[i]:''))
+			}
+			window._param=(paramObj);
+
+
+
+			var xhr= new XMLHttpRequest();
+			xhr.open('GET',obj.dom+'.html',true);
+			xhr.send(null);
+			xhr.onreadystatechange=function(){
+
+				if (xhr.readyState==4)
+				{// 4 = "loaded"
+				  if (xhr.status==200)
+				    {// 200 = OK
+				    	//console.log(xhr.responseText);
+				    	//main.write(xhr.responseText);
+				    	//var section=document.createElement('section');
+				    	//section.innerHTML=xhr.responseText;
+				    	main.innerHTML=(xhr.responseText);
+				    	var script=document.createElement('script')
+				    	script.src='/js/'+obj.script;
+				    	main.appendChild(script);
+				    	//main.appendChild(section);
+				    }
+				}
+
+
+			}
+		}
+	}
+}
+
+
+window.addEventListener("popstate",function(e){
+	window.linkTo();
+});
---
+++ routes/system.js
@@ -0,0 +1,30 @@
+var express = require('express');
+let crypto = require('crypto');
+var router = express.Router();
+
+let aesMethod=(token,callback)=>{
+  token=token.replace(' ','+')
+  let key='ZXSWEDCSFRTGBGFW'
+  let iv='8765432176547657'
+  var crypted = new Buffer(token, 'base64').toString('binary')
+
+  var decipher = crypto.createDecipheriv('aes-128-cbc', key, iv)
+  console.log(decipher)
+  var decoded = decipher.update(crypted, 'binary', 'utf8')
+  decoded += decipher.final('utf8')
+  callback(decoded)
+}
+
+/* GET users listing. */
+router.get('/getUser', (req, res, next)=>{
+  let token=req.query.token.replace(' ','+')
+  let user=JSON.parse(decoded)
+  aesMethod(token,(decoded)=>{
+    res.json({success:0,str:decoded,data:{}})
+  })
+  
+});
+
+
+
+module.exports = router;
