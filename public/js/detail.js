dc563ff66f5519f4ff5e26147554de802961bbef
xjqGit <xjq_changjiu@163.com>
2018-06-21 18:20:59 +0800

    addjs

--- public/js/detail.js
+++ public/js/detail.js
@@ -17,3 +17,100 @@ $(function(){
 		$(".unlike_btn").show();
 	})
 })
+
+	var vapp = new Vue({
+		el:".detail_main",
+		data:{
+			news:[],
+			memberId:_param.id,
+			mInfo:{
+				education:""
+			}
+		},
+		methods:{
+			getMemberInfo:function(){
+				var getUrl = "dating_api/detail?id="+this.memberId;
+				this.$http.get(getUrl).then(function(data){
+					var dat = data.data;
+					if(typeof dat=="string"){
+						dat = JSON.parse(data.data);
+					}
+					this.news = dat.data;
+					this.getEdu();
+				});
+			},
+			getAge:function(birthday){         
+			    var returnAge;  
+			    var sArr = birthday.split("T"); 
+			    var birthArr = sArr[0].split("-");
+			    var birthYear = birthArr[0];  
+			    var birthMonth = birthArr[1];  
+			    var birthDay = birthArr[2];  
+			      
+			    d = new Date();  
+			    var nowYear = d.getFullYear();  
+			    var nowMonth = d.getMonth() + 1;  
+			    var nowDay = d.getDate();  
+			      
+			    if(nowYear == birthYear){  
+			        returnAge = 0;//同年 则为0岁  
+			    }  
+			    else{  
+			        var ageDiff = nowYear - birthYear ; //年之差  
+			        if(ageDiff > 0){  
+			            if(nowMonth == birthMonth) {  
+			                var dayDiff = nowDay - birthDay;//日之差  
+			                if(dayDiff < 0){  
+			                    returnAge = ageDiff - 1;  
+			                }else{  
+			                    returnAge = ageDiff ;  
+			                }  
+			            }else{  
+			                var monthDiff = nowMonth - birthMonth;//月之差  
+			                if(monthDiff < 0){  
+			                    returnAge = ageDiff - 1;  
+			                }else{  
+			                    returnAge = ageDiff ;  
+			                }  
+			            }  
+			        }else{  
+			            returnAge = -1;//返回-1 表示出生日期输入错误 晚于今天  
+			        }  
+			    }  
+			    return returnAge;//返回周岁年龄  
+			      
+			},
+			getBirth:function(birthday){
+			    var sArr = birthday.split("T"); 
+			    var birthArr = sArr[0].split("-");
+			    var birthYear = birthArr[0];  
+			    var birthMonth = birthArr[1];  
+			    var birthDay = birthArr[2];
+			    var retBirthday = birthYear+"."+birthMonth+"."+birthDay;
+			    return retBirthday;
+			},
+			getEdu:function(){
+				var eduNum = this.news.education;
+				if(eduNum == 0){
+					this.mInfo.education = "其他";
+				}else if(eduNum == 1){
+					this.mInfo.education = "小学";
+				}else if(eduNum == 2){
+					this.mInfo.education = "初中";
+				}else if(eduNum == 3){
+					this.mInfo.education = "高中";
+				}else if(eduNum == 4){
+					this.mInfo.education = "中专";
+				}else if(eduNum == 5){
+					this.mInfo.education = "大学专科";
+				}else if(eduNum == 6){
+					this.mInfo.education = "大学本科";
+				}else if(eduNum == 7){
+					this.mInfo.education = "研究生";
+				}
+				console.log("education:"+this.mInfo.education);
+			}
+			
+		}
+	});
+	vapp.getMemberInfo();
--- public/js/list.js
+++ public/js/list.js
@@ -6,9 +6,83 @@ $(function(){
 		if(like_flag=="true"){
 			$(this).attr("src","/img/unlike.png");
 			$(this).attr("data-like","false");
+			console.log("yes");
 		}else{
 			$(this).attr("src","/img/like.png");
 			$(this).attr("data-like","true");
+			console.log("no");
 		}
 	});
+	
+	$(".member_dl").click(function(){
+		window.location.href = "#detail";
+	})
+	
 })
+
+	var vapp = new Vue({
+		el:'.list_main',
+		data:{
+			news:[]
+		},
+		methods:{
+			ready:function(){
+				var getUrl = 'dating_api/list?page=1&size=5&order_type=asc&order=day_of_birth';
+				this.$http.get(getUrl).then(function(data){
+					var dat = data.data;
+					if(typeof dat == 'string'){
+	                    dat=JSON.parse(data.data);
+	                }
+					this.news = dat.data;
+//					console.log(this.news.day_of_birth);
+					console.log("success:"+dat.success);
+					console.log("message:"+dat.message);
+				});
+			},
+			getAge:function(birthday){         
+			    var returnAge;  
+			    var sArr = birthday.split("T"); 
+			    var birthArr = sArr[0].split("-");
+			    var birthYear = birthArr[0];  
+			    var birthMonth = birthArr[1];  
+			    var birthDay = birthArr[2];  
+			      
+			    d = new Date();  
+			    var nowYear = d.getFullYear();  
+			    var nowMonth = d.getMonth() + 1;  
+			    var nowDay = d.getDate();  
+			      
+			    if(nowYear == birthYear){  
+			        returnAge = 0;//同年 则为0岁  
+			    }  
+			    else{  
+			        var ageDiff = nowYear - birthYear ; //年之差  
+			        if(ageDiff > 0){  
+			            if(nowMonth == birthMonth) {  
+			                var dayDiff = nowDay - birthDay;//日之差  
+			                if(dayDiff < 0){  
+			                    returnAge = ageDiff - 1;  
+			                }else{  
+			                    returnAge = ageDiff ;  
+			                }  
+			            }else{  
+			                var monthDiff = nowMonth - birthMonth;//月之差  
+			                if(monthDiff < 0){  
+			                    returnAge = ageDiff - 1;  
+			                }else{  
+			                    returnAge = ageDiff ;  
+			                }  
+			            }  
+			        }else{  
+			            returnAge = -1;//返回-1 表示出生日期输入错误 晚于今天  
+			        }  
+			    }  
+			    return returnAge;//返回周岁年龄  
+			      
+			},  
+			getId:function(mId){
+				window.location.href="#detail/"+mId;
+			}
+		}
+	});
+	vapp.ready();
