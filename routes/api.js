99f8e05aad2c47c747455c6e3194ffff6353af1c
xiunaonao <onlymyalways@163.com>
2018-06-25 19:24:04 +0800

    openid

--- routes/api.js
+++ routes/api.js
@@ -9,9 +9,11 @@ var storage = multer.diskStorage({
      //设置上传后文件路径，uploads文件夹会自动创建。
         destination: function (req, file, cb) {
             cb(null, './uploads_temp')
+           	cb(null,'./public/temp')
        }, 
      //给上传文件重命名，获取添加后缀名
       filename: function (req, file, cb) {
+
           var fileFormat = (file.originalname).split(".");
           cb(null, file.fieldname + '-' + Date.now() + "." + fileFormat[fileFormat.length - 1]);
       }
