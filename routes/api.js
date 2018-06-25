7ebcedb2368a7369ddf1872ca20fa6dcfd44e4d0
xiunaonao <onlymyalways@163.com>
2018-06-25 19:09:18 +0800

    openid

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,24 @@ var router = express.Router();
 var fs=require('fs');
 
 var multer=require("multer")
-var upload=multer({ dest: 'uploads_temp/' })
+//var upload=multer({ dest: 'uploads_temp/' })
+
+var storage = multer.diskStorage({
+     //设置上传后文件路径，uploads文件夹会自动创建。
+        destination: function (req, file, cb) {
+            cb(null, './uploads_temp')
+       }, 
+     //给上传文件重命名，获取添加后缀名
+      filename: function (req, file, cb) {
+          var fileFormat = (file.originalname).split(".");
+          cb(null, file.fieldname + '-' + Date.now() + "." + fileFormat[fileFormat.length - 1]);
+      }
+ });  
+     //添加配置文件到muler对象。
+     var upload = multer({
+          storage: storage
+    });
+
 
 router.post('/upload',upload.any(),(req,res,next)=>{
 	console.log(req.files)
