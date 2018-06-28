c8ba7213140e32f64057bb9bec15b065cdc33cd4
xiunaonao <onlymyalways@163.com>
2018-06-28 11:10:45 +0800

    xq

--- public/js/list.js
+++ public/js/list.js
@@ -58,7 +58,7 @@ jQuery(function(){
 				}else{
 					var listArr = JSON.parse(sessionStorage.listPost);
 					console.log(listArr.age);
-					var getUrl = 'dating_api/list?page=1&size=20&order_type=asc&order=day_of_birth&age='+listArr.age+'&height='+listArr.height+'&education='+listArr.education+'&annual_income='+listArr.annual_income+'&housing='+listArr.housing+'&car_buying='+listArr.car_buying;
+					var getUrl = 'dating_api/list?page=1&size=20&order_type=desc&order=day_of_birth&age='+listArr.age+'&height='+listArr.height+'&education='+listArr.education+'&annual_income='+listArr.annual_income+'&housing='+listArr.housing+'&car_buying='+listArr.car_buying;
 					this.$http.get(getUrl).then(function(data){
 						var dat = data.data;
 						if(typeof dat == 'string'){
