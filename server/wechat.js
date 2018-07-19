70ff335f33137dd6f7056b23c1f97ecdd34d9202
xiunaonao <onlymyalways@163.com>
2018-07-19 13:59:15 +0800

    wechatInfo

--- server/wechat.js
+++ server/wechat.js
@@ -15,7 +15,7 @@ let post_one=(obj)=>{
 		"template_id":"9cWnla5OOXHC2-dHJ-xkpF5eqUedQuUQDZNzJT5xrgk",
 		"url":"http://100579.un.123zou.com/member/PreImmigrate",
 		"miniprogram":{
-			"appid":appid,
+			"appid":"",
 			"pagepath":""
 		},
 		"data":{
