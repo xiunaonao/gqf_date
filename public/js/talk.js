92202c776b596e376e8c7a526ef227c74db12a83
xiunaonao <onlymyalways@163.com>
2018-08-14 11:11:23 +0800

    留言完成

--- public/js/talk.js
+++ public/js/talk.js
@@ -13,7 +13,17 @@ var vapp=new Vue({
 		},
 		add_message:function(){
 			var scope=this;
-			axios.post('/dating_api/insert_or_update_message',{message:scope.message})
+			if(!this.message){
+				_alert('请输入内容')
+				return;
+			}
+			axios.post('/dating_api/insert_or_update_message',{message:scope.message}).then(function(res){
+				if(res.data.success){
+					_alert('留言已发布，待管理员审核')
+				}else{
+					_alert('发布失败')
+				}
+			})
 		}
 	},
 	mounted:function(){
